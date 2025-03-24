690d1e1310b573f79833f060e42be43b1454779a
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-24 18:53:14 +0300

    Base

---
+++ BlazorWebRtc_Domain/Common/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorWebRtc_Domain.Common;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+}
