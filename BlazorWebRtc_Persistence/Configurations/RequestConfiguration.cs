e366169740cb3c5045c521a06472bfb456b1a07b
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-16 16:10:19 +0300

    Configurations added

---
+++ BlazorWebRtc_Persistence/Configurations/RequestConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using BlazorWebRtc_Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BlazorWebRtc_Persistence.Configurations;
+
+internal class RequestConfiguration : IEntityTypeConfiguration<Request>
+{
+    public void Configure(EntityTypeBuilder<Request> builder)
+    {
+        builder
+            .HasOne(s => s.SenderUser)
+            .WithMany()
+            .HasForeignKey(s => s.SenderUserId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder
+            .HasOne(s => s.ReceiverUser)
+            .WithMany()
+            .HasForeignKey(s => s.ReceiverUserId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
---
+++ BlazorWebRtc_Persistence/Configurations/UserFriendConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using BlazorWebRtc_Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BlazorWebRtc_Persistence.Configurations;
+
+internal class UserFriendConfiguration : IEntityTypeConfiguration<UserFriend>
+{
+    public void Configure(EntityTypeBuilder<UserFriend> builder)
+    {
+        builder
+            .HasOne(s => s.Requester)
+            .WithMany()
+            .HasForeignKey(s => s.RequesterId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder
+            .HasOne(s => s.ReceiverUser)
+            .WithMany()
+            .HasForeignKey(s => s.ReceiverUserId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
