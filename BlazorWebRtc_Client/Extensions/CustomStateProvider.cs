9d45edb5eaa85efad435e0756d6bee26e74b2270
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-16 16:11:46 +0300

    Extensions added

---
+++ BlazorWebRtc_Client/Extensions/CustomStateProvider.cs
@@ -0,0 +1,47 @@
+﻿using Blazored.LocalStorage;
+using BlazorWebRtc_Client.Constants;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+
+namespace BlazorWebRtc_Client.Extensions;
+
+public class CustomStateProvider : AuthenticationStateProvider
+{
+    private readonly HttpClient _httpClient;
+    private readonly ILocalStorageService _localStorageService;
+
+    public CustomStateProvider(HttpClient httpClient, ILocalStorageService localStorageService)
+    {
+        _httpClient = httpClient;
+        _localStorageService = localStorageService;
+    }
+
+
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        String? token = await _localStorageService.GetItemAsync<string>(BaseConstants.LocalToken);
+
+        if (token is null)
+            return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+
+        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", token);
+        return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(JwtParser.ParseJwtToClaim(token), "jwtAuthType")));
+    }
+
+
+    // Kullanıcı login olduğunda bildirim basacak
+    public void NotifyUserLoggedIn(string token)
+    {
+        var authenticatedUser = new ClaimsPrincipal(new ClaimsIdentity(JwtParser.ParseJwtToClaim(token), "jwtAuthType"));
+        var authState = Task.FromResult(new AuthenticationState(authenticatedUser));
+        NotifyAuthenticationStateChanged(authState);
+    }
+
+
+    public void NotifyUserLogout()
+    {
+        var authState = Task.FromResult(new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity())));
+        NotifyAuthenticationStateChanged(authState);
+    }
+}
---
+++ BlazorWebRtc_Client/Extensions/JwtParser.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Claims;
+using System.Text.Json;
+
+namespace BlazorWebRtc_Client.Extensions;
+
+public static class JwtParser
+{
+    public static IEnumerable<Claim> ParseJwtToClaim(string jwt)
+    {
+        List<Claim> claims = new();
+        String payload = jwt.Split(".")[1]; // Payload Data
+        Byte[] jsonBytes = ParseBase64WithoutPadding(payload);
+        Dictionary<string, object>? keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+        claims.AddRange(keyValuePairs!.Select(c => new Claim(c.Key, c.Value.ToString()!)));
+        return claims;
+    }
+
+    private static byte[] ParseBase64WithoutPadding(string base64)
+    {
+        switch (base64.Length % 4)
+        {
+            case 2: base64 += "=="; break;
+            case 3: base64 += "="; break;
+        }
+        return Convert.FromBase64String(base64);
+    }
+}
