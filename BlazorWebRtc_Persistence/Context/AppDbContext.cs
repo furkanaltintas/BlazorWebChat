d14782c24bcf2ab75f370a4b730b3b18b1cb8b52
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-16 16:10:35 +0300

    Context added

---
+++ BlazorWebRtc_Persistence/Context/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using BlazorWebRtc_Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorWebRtc_Persistence.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected AppDbContext()
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(Ioc).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+    public DbSet<MessageRoom> MessageRooms { get; set; }
+    public DbSet<Request> Requests { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserFriend> UserFriends { get; set; }
+}
