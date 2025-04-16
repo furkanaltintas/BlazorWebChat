9f6b5c552de6f8d3568556048c73c47d7d641e96
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-16 16:10:42 +0300

    Persistence

---
+++ BlazorWebRtc_Persistence/Ioc.cs
@@ -0,0 +1,15 @@
+﻿using BlazorWebRtc_Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazorWebRtc_Persistence;
+
+public static class Ioc
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        return services;
+    }
+}
