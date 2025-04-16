0d172e5a2ff1497948e98ae78a5480fa0375be54
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-16 16:13:49 +0300

    Upload added

---
+++ BlazorWebRtc_Application/Features/Commands/Upload/UploadCommand.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+
+namespace BlazorWebRtc_Application.Features.Commands.Upload;
+
+public class UploadCommand : IRequest<bool>
+{
+    public string UserId { get; set; }
+    public string File { get; set; }
+
+    public UploadCommand()
+    {
+        UserId = string.Empty;
+        File = string.Empty;
+    }
+
+    public UploadCommand(string userId, string file)
+    {
+        UserId = userId;
+        File = file;
+    }
+}
---
+++ BlazorWebRtc_Application/Features/Commands/Upload/UploadCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using BlazorWebRtc_Domain.Entities;
+using BlazorWebRtc_Persistence.Context;
+using MediatR;
+
+namespace BlazorWebRtc_Application.Features.Commands.Upload;
+
+public class UploadCommandHandler : IRequestHandler<UploadCommand, bool>
+{
+    private readonly AppDbContext _context;
+    public UploadCommandHandler(AppDbContext context) { _context = context; }
+
+    public async Task<bool> Handle(UploadCommand request, CancellationToken cancellationToken)
+    {
+        User? user = await _context.Users.FindAsync(request.UserId, cancellationToken);
+        if(user is null) return false;
+
+        user.Profile = request.File;
+        await _context.SaveChangesAsync();
+        return true;
+    }
+}
