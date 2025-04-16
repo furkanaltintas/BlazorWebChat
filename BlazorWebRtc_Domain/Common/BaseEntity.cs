4aa6684f61c779c66642f45836ddf32f3221caf1
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-16 16:09:30 +0300

    BaseEntity modified

--- BlazorWebRtc_Domain/Common/BaseEntity.cs
+++ BlazorWebRtc_Domain/Common/BaseEntity.cs
@@ -3,5 +3,5 @@
 public abstract class BaseEntity
 {
     public Guid Id { get; set; } = Guid.NewGuid();
-    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+    public DateTime CreatedDate { get; set; } = DateTime.Now;
 }
