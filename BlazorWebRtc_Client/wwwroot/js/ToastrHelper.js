1c5e014e1f5652b6f53e99bc4d3e54a8d5ace07f
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-16 16:21:35 +0300

    Toastr added

---
+++ BlazorWebRtc_Client/wwwroot/js/ToastrHelper.js
@@ -0,0 +1,19 @@
+﻿toastr.options = {
+    "closeButton": true,
+    "progressBar": true,
+    "positionClass": "toast-bottom-right",
+    "timeOut": "3000"
+};
+
+window.toastrNotify = (type, message, title) => {
+    console.log("trigger toastrNotify")
+    if (type === "success") {
+        toastr.success(message, title);
+    } else if (type === "error") {
+        toastr.error(message, title);
+    } else if (type === "info") {
+        toastr.info(message, title);
+    } else if (type === "warning") {
+        toastr.warning(message, title);
+    }
+};
